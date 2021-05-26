c5416abe86ccdaf9f526571def8cbb63a9354d0d
k45hish <k.mavani123@gmail.com>
2021-05-26 15:29:56 -0700

    task 1.7

---
+++ T1, T2/task 1.7.py
@@ -0,0 +1,6 @@
+VariableNumberOne = 'Upper Camel Case'
+variableNumberTwo = 'lower Camel Case'
+variable_number_three = 'snake_case'
+VARIABLENUMBERFOUR  = 'UPPERCASE'
+print (VariableNumberOne + '\n' + variableNumberTwo + '\n' + variable_number_three + '\n' + VARIABLENUMBERONE)
+
