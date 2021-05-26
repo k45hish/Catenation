eaada6a21a975bec28ed072e738971cfba40d16a
k45hish <k.mavani123@gmail.com>
2021-05-26 14:02:07 -0700

    .

--- VsCode/task 2.1.py
+++
@@ -1,7 +0,0 @@
-x = int(input('enter value : '))
-if x%3==0 and x%5==0:
-      print('consultadd-python training')
-elif x%5==0:
-      print('python training')
-elif x%3==0:
-      print('consultadd')
